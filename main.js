adb1abeea1cfb00e35dbcb52b3a580c8fe9e8408
William Kimball <wkkimball043@gmail.com>
2018-06-16 15:52:37 -0500

    finished this exercise

--- main.js
+++ main.js
@@ -24,11 +24,13 @@ const DragDropManager = Object.create(null, {
           // Determine what's being dropped
           const data = e.dataTransfer.getData("text")
 
+          console.log(event.target.tagName)
           // Append card to target component as child
           // TODO: This should only happen if the target has no children nodes
           // TODO: This should not happen if the target is another stage card
-
+          if (e.target.hasChildNodes() == false || e.target.tagName == "ARTICLE" ){
           e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+          }
         }
       })
     }
