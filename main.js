039f97e9006b14658bcc025cdd663de004d82e30
William Kimball <wkkimball043@gmail.com>
2018-06-18 14:31:43 -0500

    fixed bug where you could not drag and drop on cards in middle div

--- main.js
+++ main.js
@@ -22,14 +22,15 @@ const DragDropManager = Object.create(null, {
           e.preventDefault()
 
           // Determine what's being dropped
-          const data = e.dataTransfer.getData("text")
-
-          console.log(event.target.tagName)
+          const data = e.dataTransfer.getData("text");
           // Append card to target component as child
           // TODO: This should only happen if the target has no children nodes
           // TODO: This should not happen if the target is another stage card
           if (e.target.hasChildNodes() == false || e.target.tagName == "ARTICLE" ){
           e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+          } else if (e.target.hasChildNodes() == true && e.target.parentNode.tagName == "ARTICLE" ) {
+            let parent = e.target.parentNode;
+            parent.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
           }
         }
       })
